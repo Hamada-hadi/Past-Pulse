7f0dc5fa1e5a414b9678e5b75fa7fca42d7dc772
Hamada-hadi <ahmedgazow@gmail.com>
2023-12-26 12:41:24 -0700

    After Deploying

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -5,11 +5,11 @@ export default (posts = [], action) => {
     case FETCH_ALL:
       return action.payload;
     case LIKE:
-      return posts.like.map((post) => (post._id === action.payload._id ? action.payload : post));
+      return posts.data.map((post) => (post._id === action.payload._id ? action.payload : post));
     case CREATE:
       return [...posts, action.payload];
     case UPDATE:
-      return posts.update.map((post) => (post._id === action.payload._id ? action.payload : post));
+      return posts.data.map((post) => (post._id === action.payload._id ? action.payload : post));
     case DELETE:
       return posts.filter((post) => post._id !== action.payload);
     default:
