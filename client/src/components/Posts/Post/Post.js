b9cba734d5e387042c9ccd5c613f8a4fcd56eaf5
Hamada-hadi <ahmedgazow@gmail.com>
2023-12-26 03:11:52 -0700

    After Deploying

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -43,7 +43,7 @@ const Post = ({ post, setCurrentId }) => {
       </div>
       )}
       <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map[(tag) => `#${tag} `]}</Typography>
+        <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
       </div>
       <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
       <CardContent>
